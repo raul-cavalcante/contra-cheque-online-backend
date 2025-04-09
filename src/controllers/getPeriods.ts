aaa950c47cc8d7161b3ac2538570074a081ff8d5
Raul Cavalcante <raulcavalcante.a@gmail.com>
2025-04-09 19:33:05 -0300

    add type

--- src/controllers/getPeriods.ts
+++ src/controllers/getPeriods.ts
@@ -7,7 +7,7 @@ export const getAvailablePeriods= async (req: ExtendedRequest, res: Response) =>
     res.status(401).json({error: 'Não autorizado'});
     return;
   }
-  const user = await getPeriodsService(req.userId, req.month, req.year);
+  const user = await getPeriodsService(req.userId, Number(req.month), Number(req.year));
   
   if(!user){
     res.status(404).json({error: 'Nenhum contra-cheque encontrado'});
